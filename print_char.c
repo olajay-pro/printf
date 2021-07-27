f0f753bd218673efd412b5b15f33f4ce16ce193a
logicSt <olumide.f.akinduko@gmail.com>
2021-07-27 06:12:39 -0700

    fixed betty

--- print_char.c
+++ print_char.c
@@ -10,6 +10,7 @@
 int print_c(va_list c)
 {
 char ch = (char)va_arg(c, int);
+
 _putchar(ch);
 return (1);
 }
@@ -24,6 +25,7 @@ int print_s(va_list s)
 {
 int count;
 char *str = va_arg(s, char *);
+
 if (str == NULL)
 str = "(null)";
 for (count = 0; str[count]; count++)
@@ -44,6 +46,7 @@ static int hex_print(char c)
 int count;
 char diff = 'A' - ':';
 char d[2];
+
 d[0] = c / 16;
 d[1] = c % 16;
 for (count = 0; count < 2; count++)
@@ -52,6 +55,7 @@ if (d[count] >= 10)
 _putchar('0' + diff + d[count]);
 else
 _putchar('0' + d[count]);
+}
 return (count);
 }
 
@@ -66,6 +70,7 @@ int print_S(va_list S)
 unsigned int i;
 int count = 0;
 char *str = va_arg(S, char *);
+
 if (str == NULL)
 str = "(null)";
 for (i = 0; str[i]; i++)
@@ -96,6 +101,7 @@ int print_r(va_list r)
 {
 char *str;
 int i, count = 0;
+
 str = va_arg(r, char *);
 if (str == NULL)
 str = ")llun(";
